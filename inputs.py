33c7733a2f4a99563184c5fcb523c6f3a0ecd15a
Marjan Moderc <marjan.moderc@gmail.com>
2017-08-24 15:26:47 +0200

    delete dummy requirements.txt

--- inputs.py
+++ inputs.py
@@ -44,6 +44,13 @@ if CREATION_METHOD == "auto":
     PROFILE_WIDTH = 100
 
 
+# 4.2. CREATION_METHOD == "lines":
+if CREATION_METHOD == "lines":
+    # Specify the location of the LineString shapefile (*.shp!), representing locations of desired cross sections:
+    XSECTION_SHP = "/home/marjan/arso/frisco/clanek/test_model/Sotla_profili.shp"
+
+
+
 # 4.2. CREATION_METHOD == "measurements":
 if CREATION_METHOD == "measurements":
     # Specify the location of the Point shapefile (*.shp!), representing locations of the profile measurements.
@@ -58,21 +65,18 @@ if CREATION_METHOD == "measurements":
     # Specify the column name, containing the height of the measured point
     Z_FIELD= "z_LAS"
 
-# 4.2. CREATION_METHOD == "lines":
-if CREATION_METHOD == "lines":
-    # Specify the location of the LineString shapefile (*.shp!), representing locations of desired cross sections:
-    XSECTION_SHP = "/home/marjan/arso/frisco/clanek/test_model/Sotla_profili.shp"
-
 
 # 5. RESULT FILES - ALL ARE OPTIONAL!
+
+# 5.1. Specify the absolute path of the Mike 11 XNS import cross section_file! (defaults to xns11_ready.txt)
+XNS11_OUT_TXT = "Mike11_xns_ready.txt"
+
 # Specify optional point sampling of the riverlines...
 RIVER_POINT_OUT_SHP = "/home/marjan/arso/frisco/static/shp_source/Dragonja_reke_d96_vzdolzni.shp"
 
 # Specify the absolute path of the result point file of the point sampled embankments file
 EMBANKMENTS_POINT_OUT_SHP = None
 
-# 5.1. Specify the absolute path of the Mike 11 XNS import cross section_file! (defaults to xns11_ready.txt)
-XNS11_OUT_TXT = "Mike11_xns_ready.txt"
 
 # 5.2. Specify the absolute path of the result XNS point(or lines) shapefile if you want it! (defaults to None)
 XSECTION_POINTS_OUT_SHP = "/home/marjan/arso/frisco/static/shp_source/Dragonja_profili_d96_test_points.shp"
